ce8bc813588e821290c0fcfe4c7b372029a99d46
nlampetis <nlampetis@gmail.com>
2023-02-23 22:55:22 +0200

    initial code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       logger.info("Hello world!");
+
+       MainLayout mainLayout = new MainLayout();
+       mainLayout.setContentPane(mainLayout.getMainPanel());
+       mainLayout.setTitle("App");
+       mainLayout.setSize(800, 300);
+       mainLayout.setVisible(true);
+       mainLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
--- src/main/java/org/example/MainLayout.java
+++ src/main/java/org/example/MainLayout.java
@@ -1,2 +1,25 @@
-package org.example;public class MainLayout {
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainLayout extends JFrame{
+    private JButton button1;
+    private JTree tree1;
+    private JLabel label;
+    private JPanel mainPanel;
+
+    public MainLayout() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
 }
